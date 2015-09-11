e9042397321ba2696ca7667007da78aca60c6451
caguthrie <christopher.a.guthrie@gmail.com>
2015-09-11 15:16:09 -0400

    Closes erfangc/DecoratedStockChart#25

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,24 @@ const rename = require("gulp-rename");
 const templateCache = require('gulp-angular-templatecache');
 const pkg = require('./package.json');
 
-gulp.task('default', ['css', 'templates', 'uglify'], function () {
+gulp.task('default', ['css'], function () {
     console.log("done building project " + pkg.name);
 });
 
-gulp.task('templates', templateCacheCB);
-
-gulp.task('css', function () {
+gulp.task('css', ['templates'], function () {
     return gulp
         .src('src/**/*.css')
         .pipe(concat(pkg.name + '.css'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('templates', ['uglify'], templateCacheCB);
+
+gulp.task('uglify', ['concat'], function () {
+    return gulp
+        .src('dist/' + pkg.name + '.js')
+        .pipe(uglify({mangle: false}))
+        .pipe(rename(pkg.name + '.min.js'))
         .pipe(gulp.dest('dist/'));
 });
 
@@ -22,14 +30,6 @@ gulp.task('concat', function () {
     return gulp
         .src(['src/**/*.js'])
         .pipe(concat(pkg.name + ".js"))
-        .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('uglify', ['concat'], function () {
-    return gulp
-        .src('dist/' + pkg.name + '.js')
-        .pipe(uglify({mangle: false}))
-        .pipe(rename(pkg.name + '.min.js'))
         .pipe(gulp.dest('dist/'));
 });
 
